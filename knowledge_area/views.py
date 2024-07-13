4a97044093eb77436911133e30e2cf092ead7893
davipythonweb <galaxys3tab32@gmail.com>
2024-07-13 03:22:55 -0300

    update  app knowledge_area `views.py`

--- knowledge_area/views.py
+++ knowledge_area/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+"""
+trilha do projeto.
+
+area de conhecimento = knowledge_area  #equivalente a {genres}
+
+livros = books #equivalente a {movies}
+
+assuntos = matters #equivalente a {actors}
+
+avaliações = reviews #equivalente a {reviews}
+(choices com uma lista com as estrelas possiveis para avaliar.)
+ 
+"""
+
 def knowledge_area(request):
     return HttpResponse('Books_API')
 
