cf256080d852e55cebc117e62a9794cb826ed83d
davipythonweb <galaxys3tab32@gmail.com>
2024-07-23 14:36:29 -0300

    create models do app `matters`

--- matters/models.py
+++ matters/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+NATIONALITY_CHOICES =(
+    ('USA', 'Estados Unidos'),
+    ('BRA', 'Brasil'),
+    ('FRA', 'França'),
+    ('COL', 'Colombia'),
+    ('CHN', 'China'),
+    ('JPN', 'Japão'),
+    ('NLD', 'Holanda'),
+
+)
+
+class Matter(models.Model):
+    matter = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    nationality = models.CharField(
+        max_length=20,
+        choices=NATIONALITY_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self) -> str:
+        return self.matter
