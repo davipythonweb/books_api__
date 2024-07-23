3377b4eba0a5cb4fbd5eaf13175153d134324274
davipythonweb <galaxys3tab32@gmail.com>
2024-07-23 15:06:18 -0300

    update views and urls of app `matters`

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('knowledge_area.urls')),
+    path('api/v1/', include('matters.urls')),
 ]
--- matters/views.py
+++ matters/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from matters.models import Matter
 
+from matters.serializers import MatterSerializer
 
 class MatterCreateListView(generics.ListCreateAPIView):
     queryset = Matter.objects.all()
-    
+    serializer_class = MatterSerializer
