7363a17597547ea8e9c2672cd2fb5fe1c2f68e03
davipythonweb <galaxys3tab32@gmail.com>
2024-07-23 23:14:43 -0300

    update urls and views of app `matters`

--- matters/urls.py
+++ matters/urls.py
@@ -5,4 +5,6 @@ app_name = 'namespaceMatter'
 
 urlpatterns = [
     path('matters/', views.MatterCreateListView.as_view(), name='list'),
+    
+    path('matters/<int:pk>/' , views.MatterRetrieveUpdateDestroyView.as_view(), name='retrieve'),
 ]
--- matters/views.py
+++ matters/views.py
@@ -3,6 +3,11 @@ from matters.models import Matter
 
 from matters.serializers import MatterSerializer
 
+
 class MatterCreateListView(generics.ListCreateAPIView):
+    queryset = Matter.objects.all()
+    serializer_class = MatterSerializer
+
+class MatterRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Matter.objects.all()
     serializer_class = MatterSerializer
