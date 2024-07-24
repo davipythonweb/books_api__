28634f62d16cf541272c79a7ef1c9ddc37d0cfbe
davipythonweb <galaxys3tab32@gmail.com>
2024-07-24 01:08:05 -0300

    create models of app `books`

--- books/models.py
+++ books/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from matters.models import Matter
+from knowledge_area.models import Knowledge_Area
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    knowledge_area = models.ForeignKey(
+        Knowledge_Area,
+        on_delete=models.PROTECT,
+        related_name='books'
+    )
+    release_date = models.DateField(
+        null=True,
+        blank=True
+    )
+    matter = models.ManyToManyField(
+        Matter,
+        related_name='books'
+    )
+    resume = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
