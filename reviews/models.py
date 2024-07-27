bad6ec0fa5e6f7ff5ff754688432f4f0f223d1f2
davipythonweb <galaxys3tab32@gmail.com>
2024-07-27 01:00:10 -0300

    create models of app `reviews`

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from books.models import Book
 
-# Create your models here.
+
+STARS_FOR_REVIEW = (
+    ('1', '★'),
+    ('2', '★★'),
+    ('3', '★★★'),
+    ('4', '★★★★'),
+    ('5', '★★★★★'),
+)
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.PROTECT,
+        related_name='reviews'
+    )
+
+    stars = models.CharField(
+        max_length=10,
+        choices=STARS_FOR_REVIEW,
+        blank=True,
+        null=True,
+    )
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.book
